afba48458ec75061a57cf3374cc5129954b2916f
br1s1ngr <oalashe@yahoo.com>
2016-09-22 11:41:56 +0100

    added start up console app. with code to initialize server.

---
+++ StartUpApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StartUpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello....Welcome");
+            Console.WriteLine("Starting........");
+            Console.WriteLine("***********************");
+
+            TCP_Server.Server server = new TCP_Server.Server();
+
+            Thread newThrd = new Thread(new ThreadStart(server.Connect));
+            newThrd.Start();
+        }
+    }
+}
--- TCP_Socket/Server.cs
+++ TCP_Socket/Server.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace TCP_Socket
+namespace TCP_Server
 {
     public class Server
     {
